df0aa228690f429d26c7467d81c26abdd0ffc142
JoaoA13 <joaoarthur.vieira@estudante.ufjf.br>
2025-07-23 18:47:55 -0300

    gte loja com cpf do gerente

--- src/main/java/SistemaDeGerenciamentoDeFranquias/GerenciadorDeLojas.java
+++ src/main/java/SistemaDeGerenciamentoDeFranquias/GerenciadorDeLojas.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class GerenciadorDeLojas {
 
-    static int quantidadeDeLojas = 0;
     static private Map<String, Loja > armazenaLojas = new HashMap<>();
     static private Map<String, Gerente > armazenaGerentes = new HashMap<>();
 
@@ -13,21 +12,15 @@ public class GerenciadorDeLojas {
 
     protected Map<String, Loja> getLojas() {return armazenaLojas;}
 
-    static protected void addLoja(String codigo,Loja loja){
-        armazenaLojas.put(codigo,loja);
-        quantidadeDeLojas++;
+    static protected void addLoja(String cpfGerente,Loja loja){
+        armazenaLojas.put(cpfGerente,loja);
     }
 
-    protected Loja getLoja(String codigo){return armazenaLojas.get(codigo);}
+    protected Loja getLoja(String cpfGerente){return armazenaLojas.get(cpfGerente);}
 
     protected static void cadastraLoja(String endereco, Gerente gerente){
-        Loja loja = new Loja(geraCodigoLoja(),endereco,gerente);
-        addLoja(loja.getCodigo(),loja);
-    }
-
-    static protected String geraCodigoLoja(){
-        String codigo = String.format("%03d", quantidadeDeLojas);
-        return codigo;
+        Loja loja = new Loja(endereco,gerente);
+        addLoja(gerente.getCpf(),loja);
     }
 
     protected static void cadastraGerente(String cpf, Gerente gerente){
